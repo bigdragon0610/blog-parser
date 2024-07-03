b9a0a5b73f91da294d91a638a91aab744b3bc6d2
ryudai <bigdragon.06102001@gmail.com>
2024-07-04 03:08:30 +0900

    compactのテストを追加

--- src/view.rs
+++ src/view.rs
@@ -18,3 +18,38 @@ macro_rules! compact {
 pub fn view(file_path: &str) -> String {
     fs::read_to_string(file_path).expect("Something went wrong reading the file")
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_compact() {
+        let mut html =
+            String::from("{{ $title }} - {{ $content }} - {{ $date }} - {{ $description }}");
+        let title = "Title";
+        let content = "Content";
+        let date = "Date";
+        let description = "Description";
+
+        compact!(html, title, content, date, description);
+
+        assert_eq!(html, "Title - Content - Date - Description");
+    }
+
+    #[test]
+    fn test_compact_escape() {
+        let mut output = String::from("{{ $text }}");
+        let text = "$";
+        compact!(output, text);
+        assert_eq!(output, "$");
+
+        let mut output = String::from("{{ $text }}");
+        let text = "$sample";
+        compact!(output, text);
+        assert_eq!(output, "$sample");
+
+        let mut output = String::from("{{ $text }}");
+        let text = "$$sample $$sample";
+        compact!(output, text);
+        assert_eq!(output, "$$sample $$sample");
+    }
+}
